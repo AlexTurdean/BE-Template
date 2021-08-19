2cdaeef0f673f640669f3b8ac6809e991706d58d
Alex Turdean <alex_turdean@yahoo.ro>
2021-08-19 18:34:50 +0300

    Add tests for get profile

---
+++ test/app.test.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const app = require('../src/app');
+const request = require('supertest');
+
+describe('CONTRACTS', () => {
+
+    describe('GET /contracts/:id', () => {
+
+        it('should trow an error for missing profile_id', () => {
+            return request(app).get('/contracts/1')
+            .expect(401);
+        });
+
+        it('should trow an error for non-existing profile_id', () => {
+            return request(app).get('/contracts/1')
+            .set({
+                profile_id: 10000
+            })
+            .expect(401);
+        });
+
+        it('should get a contract for a client', () => {
+            return request(app).get('/contracts/1')
+            .set({
+                profile_id: 1
+            })
+            .expect(200)
+            .then(result => {
+                assert(result.body.id == 1);
+            });
+        });
+
+        it('should get a contract for a contractor', () => {
+            return request(app).get('/contracts/1')
+            .set({
+                profile_id: 5
+            })
+            .expect(200)
+            .then(result => {
+                assert(result.body.id == 1);
+            });
+        });
+
+        it('should trow an error for trying to access others people contract', () => {
+            return request(app).get('/contracts/1')
+            .set({
+                profile_id: 2
+            })
+            .expect(403);
+        });
+
+    });
+
+});
